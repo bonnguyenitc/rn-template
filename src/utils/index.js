c34b3f3043396bba5e45c873e10967e2b9ac8aef
Nguyen Tan Thoai <thoaint@bap.jp>
2021-01-29 08:42:31 +0700

    remove not need

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 import * as Styles from './style';
 // import Firebase from './firebase';
 import * as ASStore from './async-store';
-import * as Vibration from './vibration';
 
-export default { Styles, ASStore, Vibration };
+export default { Styles, ASStore };
